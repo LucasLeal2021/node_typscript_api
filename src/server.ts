a7dd71d6b15f7e83cc12fbcdc71e0e0e78567414
Lucas <api-lucas@kollecta.io>
2026-04-08 15:44:48 -0300

    Setup prettier

--- src/server.ts
+++ src/server.ts
@@ -5,24 +5,24 @@ import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
 export class SetupServer extends Server {
-    constructor(private port: number = 3000) {
-        super();
-    }
+  constructor(private port: number = 3000) {
+    super();
+  }
 
-    public init(): void {
-        this.setupExpress();
-        this.setupControllers();
-    }
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
 
-    private setupExpress(): void {
-        this.app.use(bodyParser.json());
-    }
-    private setupControllers(): void {
-        const forecastController = new ForecastController();
-        this.addControllers([forecastController]);
-    }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+    this.addControllers([forecastController]);
+  }
 
-    public getApp(): Application {
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
